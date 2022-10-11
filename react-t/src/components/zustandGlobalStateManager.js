cd5cc2e762f6a642938db5f11b044e2a60f15a7b
Naveen <naveen.chaurasia861@gmail.com>
2022-10-11 17:04:43 +0530

    added classComponent

--- react-t/src/components/zustandGlobalStateManager.js
+++ react-t/src/components/zustandGlobalStateManager.js
@@ -23,3 +23,7 @@ function VoteCounter() {
 
 // To use Zustand, run npm install zustand. After that, make a dedicated store file or folder and create your store:
 
+// One large reason I recommend using Zustand over a library like Redux is that it gives you all the functionality you need without the boilerplate and conceptual overhead of actions, reducers, and so on.
+
+// Plus, you don’t need to wrap your components in a Context Provider. Just install and go!
+
